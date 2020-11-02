a7544b001bd559edd805f037f36baf7ca6036bb3
duongngocle96 <duongngocle96@gmail.com>
2020-11-02 14:47:26 +0700

    add code html vào wordpres

---
+++ wp-content/themes/shop/functions.php
@@ -0,0 +1,50 @@
+<?php 
+//code mặc định khi lập trình thêm woo
+
+function my_custom_wc_support(){
+		add_theme_support('woocommerce');
+		add_theme_support('wc-product-gallery-lightbox');
+		add_theme_support('wc-product-gallery-slide');
+
+	}
+
+function initTheme(){
+		//chuyển trình saọn thảo phiên bản mới về cũ
+		add_filter('use_block_editor_for_post','__return_false');
+		//đăng kí các menu..
+
+		register_nav_menu('header-main',__('menu main'));
+		register_nav_menu('header-category',__('menu categories'));
+		register_nav_menu('menu_footer',__('menu footer'));
+		//đăng kí saidebar..
+		if (function_exists('register_sidebar')){
+    		register_sidebar(array(
+			    'name'=> 'Cột bên',
+			    'id' => 'sidebar',
+		));
+		}
+}		
+	add_action('init', 'initTheme');
+
+
+
+function styles_css()
+{
+    wp_enqueue_style('custom', get_template_directory_uri() . '/libs/bootstrap/css/bootstrap.min.css', 'all');
+    wp_enqueue_style('templatemo-stand-blog', get_template_directory_uri() . '/libs/font-awesome/css/font-awesome.min.css', 'all');
+    wp_enqueue_style('fontawesome', get_template_directory_uri() . '/css/responsive.css', 'all');
+    wp_enqueue_style('owl', get_template_directory_uri() . '/css/main.css', 'all');
+}
+add_action( 'wp_enqueue_scripts', 'styles_css' );
+
+function style_js()
+{
+    //add style.js
+    wp_enqueue_script( 'jquery', get_template_directory_uri() . '/js/main.js',[], null, true);
+   
+    wp_enqueue_script( 'owl', get_template_directory_uri() . '/libs/bootstrap/js/bootstrap.min.js',[], null, true);
+    wp_enqueue_script( 'slick', get_template_directory_uri() . '/libs/jquery-3.4.1.min.js',[], null,true);
+
+}
+add_action( 'wp_enqueue_scripts', 'style_js' );
+ ?>
---
+++ wp-content/themes/shop/index.php
@@ -0,0 +1,27 @@
+<?php get_header(); ?>
+
+			<div id="content">
+				<div class="container">
+				 <?php get_template_part('slider'); ?>
+
+
+				</div>
+				<div class="product-box">
+					<div class="container">
+						<div class="row">
+							<div class="col-xs-12 col-sm-12 col-md-12 col-lg-3 order-lg-0 order-1">
+								
+              <?php get_sidebar(); ?>
+
+
+
+							</div>
+							<div class="col-xs-12 col-sm-12 col-md-12 col-lg-9 order-lg-1 order-0">
+						              <?php get_template_part('content'); ?>
+
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+<?php get_footer(); ?>
