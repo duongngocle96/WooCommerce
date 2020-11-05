63b41280bc9f501291016544c73d8e5bd4cc0210
duongngocle96 <duongngocle96@gmail.com>
2020-11-05 16:30:39 +0700

    add plugin Wp SMTP AND contact form footer

--- wp-content/themes/shop/footer.php
+++ wp-content/themes/shop/footer.php
@@ -45,26 +45,7 @@
 							<div class="box-footer info-contact">
 								<h3>Form liên hệ</h3>
 								<div class="content-contact">
-									<form action="/" method="GET" role="form">
-										<div class="row">
-											<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-												<input type="text" name="" id="" class="form-control" placeholder="Họ và Tên">
-											</div>
-											<div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
-												<input type="email" name="" id="" class="form-control" placeholder="Địa chỉ mail">
-											</div>
-											<div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
-												<input type="text" name="" id="" class="form-control" placeholder="Số điện thoại">
-											</div>
-											<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-												<input type="text" name="" id="" class="form-control" placeholder="Tiêu đề">
-											</div>
-											<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-												<textarea name="" id="" cols="30" rows="10" class="form-control"></textarea>
-											</div>
-										</div>
-										<button type="submit" class="btn-contact">Liên hệ ngay</button>
-									</form>
+									<?php echo do_shortcode('[contact-form-7 id="11" title="Contact form 1"]'); ?>
 								</div>
 							</div>
 						</div>
